05386056b5525706fb2f3b561e9d5f267c0246ba
NguyenQuan <ngquancit@gmail.com>
2017-09-13 15:02:53 +0700

    first commit

---
+++ app/Models/CarPart.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class CarPart extends Model
+{
+    protected $table="aiCarPart";
+    protected $fillable=['VehicleMake','VehicleModel','VehicleBadge','VehicleSeries'];
+
+    public function parts(){
+
+        return $this->hasMany('App\Models\Part','aiCarPartId','ID');
+
+    }
+}
